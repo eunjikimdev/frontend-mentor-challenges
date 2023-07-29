0af005dd28a38a62aeedad62628193a7e2f5cef1
Eunji Kim <rladmswl1300@naver.com>
2023-07-29 13:02:48 +0900

    Init: 처음부터 다시 작성

--- article-preview-component/js/script.js
+++ article-preview-component/js/script.js
@@ -1,14 +1,7 @@
-var shareBtn = document.querySelector(".share");
-var snsShareCard = document.querySelector(".snsShare");
+var shareBtn = document.querySelector(".share-button");
+var snsShareCard = document.querySelector(".share-option");
 
-function clickShareBtn(event) {
-  event.preventDefault();
-  const activeClass = "active";
-  if (snsShareCard.classList.contains(activeClass)) {
-    snsShareCard.classList.remove(activeClass);
-  } else {
-    snsShareCard.classList.add(activeClass);
-  }
-}
-
-shareBtn.addEventListener("click", clickShareBtn);
+shareBtn.addEventListener("click", () => {
+  shareBtn.classList.toggle("active");
+  snsShareCard.classList.toggle("active");
+});
